21ed2786bba7119c17007c0e6ae2549f849e7c0a
Jaci Cotaquispe <jc104@wellesley.edu>
2023-10-21 20:35:30 -0400

    react

---
+++ index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+export const MacbookAir = () => {
+  return (
+    <div className="macbook-air">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="rectangle" />
+          <div className="text-wrapper">Start Your Journey</div>
+        </div>
+        <img className="orpheustrans" alt="Orpheustrans" src="orpheustrans-1.png" />
+        <div className="text-wrapper-2">Orpheus</div>
+      </div>
+    </div>
+  );
+};
