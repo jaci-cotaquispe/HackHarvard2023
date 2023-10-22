9d9d924c24e57b6c1e05d93627694affb642fcf8
Jaci Cotaquispe <jc104@wellesley.edu>
2023-10-21 21:47:45 -0400

    anima

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOMClient from "react-dom/client";
-import { MacbookAir } from "./screens/MacbookAir";
-
-const app = document.getElementById("app");
-const root = ReactDOMClient.createRoot(app);
-root.render(<MacbookAir />);
-
